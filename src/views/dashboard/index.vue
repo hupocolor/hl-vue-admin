7f6ed29d221127f7c93cf3f6f6d655b1d575df34
HuposComputer <1525864549@qq.com>
2023-03-30 18:24:01 +0800

    Initial commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      欢迎使用狐狸博客管理系统
+    </div>
+    <div>
+      <a href="https://github.com/hupocolor">GitHub:https://github.com/hupocolor</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
